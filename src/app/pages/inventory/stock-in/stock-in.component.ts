6ecfd68bd8dc7250cda859ec3464798057d6d18c
jiwanbeehive <jiwan@beehiveinfotech.com>
2025-11-16 14:41:56 +0530

    update

--- src/app/pages/inventory/stock-in/stock-in.component.ts
+++ src/app/pages/inventory/stock-in/stock-in.component.ts
@@ -182,6 +182,7 @@ onSave(updatedData:any){
             }
            
             this.closeDialog(updatedData);
+          this.OnGetItem();
     }
  deleteItem(product:any) {
  
@@ -237,18 +238,22 @@ get grandTotal():number{
         this.mode='add';
         this.selectedRow=null;
         this.visibleDialog=true;
+        
         setTimeout(() => 
           this.addInventoryComp.resetForm(),10
         );
+        this.OnGetItem()
     }
     openEditDialog(rowData:any){
         this.mode='edit';
         this.selectedRow=rowData||null;
         this.visibleDialog=true;
+       
     }
     closeDialog(event:any){
       console.log(event)
         this.visibleDialog=false;
+        
         this.OnGetPurcheseItem(this.transationid)
     }
     reset(){
@@ -482,6 +487,7 @@ OnDeleteItem(id:any){
     next:(res)=>{
       this.showSuccess(res.data[0].msg)
       this.OnGetPurcheseItem(this.transationid)
+      this.OnGetItem();
       
     }
   })
