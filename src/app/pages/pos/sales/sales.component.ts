766e8f79e500b81813e89b3522f1ec766c5bc4fa
Arushi-beehive <arushi@beehiveinfotech.com>
2025-12-25 15:41:36 +0530

    sale comment remove

--- src/app/pages/pos/sales/sales.component.ts
+++ src/app/pages/pos/sales/sales.component.ts
@@ -817,44 +817,6 @@ export class SalesComponent {
     OnSalesHeaderCreate(data: any) {
         const apibody = this.cleanRequestBody(this.salesForm.value);
 
-        // const datada={
-        //
-        // "p_transactiontype": "SALE",
-        // "p_transactionid": 0,
-        // "p_transactiondate": "08/11/2025",
-        // "p_customername": "Chittaranjan",
-        // "p_mobileno": "9871757006",
-        // "p_totalcost": 1000,
-        // "p_totalsale": 1200,
-        // "p_overalldiscount": 10,
-        // "p_roundoff": "0.20",
-        // "p_totalpayable": 1080,
-        // "p_currencyid": 0,
-        // "p_gsttran": "N",
-        // "p_status": "Complete",
-        // "p_isactive": "Y",
-        //
-        // "p_linktransactionid": 0,
-        // "p_replacesimilir": "Y",
-        // "p_creditnoteno": "",
-        // "p_paymentmode": "Cash",
-        // "p_paymentdue": 0,
-        // "p_sale": [
-        //     {
-        //         "TransactiondetailId": 0,
-        //         "ItemId": 25,
-        //         "ItemName": "Switch 3 socket",
-        //         "UOMId": 1,
-        //         "Quantity":1,
-        //         "itemcost":220,
-        //         "MRP":240,
-        //         "totalPayable":240
-        //     }
-        // ],
-        // "clientcode": "CG01-SE",
-        // "x-access-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyY29kZSI6ImFkbWluIiwiaWF0IjoxNzYzNjQyOTY4LCJleHAiOjE3NjM3MjkzNjh9.2yeOGtpWD24Fl1Ske4iVv4D0yy3o_JQ1eMyaXY_Zu_U"
-
-        // }
         this.stockInService.OninsertSalesDetails(apibody).subscribe({
             next: (res) => {
                 const billno = res.data[0]?.billno;
