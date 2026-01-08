0cd41768643ed10b443cd5a6330ac3e9019ad511
surajkumararosys <skumar@arosys.com>
2026-01-08 14:11:04 +0530

    Revert "Cordova related changes"

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.beehive.cm2_app',
+  appName: 'CM2_app',
+  webDir: 'dist/cm-2/browser'
+};
+
+export default config;
